ac7c81aee4f72b4dfc855e5ea72bb500e501caae
Feerse <sungaikeledang6@gmail.com>
2024-09-12 11:01:54 +0800

    Konfigurasi Global API Endpoint

---
+++ src/api/Api.js
@@ -0,0 +1,11 @@
+// * Axios adalah library JavaScript yang digunakan untuk melakukan HTTP Request ke dalam suatu server untuk melakukan manipulasi data seperti GET, POST, PUT, DELETE. Secara default, Axios menggunakan callback response berupa "promise".
+
+// Di projek ini, axios digunakan untuk mendapatkan data dari REST API yang sudah dibuat sebelumnya di Laravel (backend-donasi)
+import axios from "axios";
+
+const Api = axios.create({
+  // Default endpoint API
+  baseURL: "http;//localhost:8000/api", // <-- Alamat baseURL dari project Laravel (backend-donasi)
+});
+
+export default Api;
