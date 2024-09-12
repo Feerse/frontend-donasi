4f8b01606a98b3c481b5683961b554f034f23468
Feerse <sungaikeledang6@gmail.com>
2024-09-12 10:44:02 +0800

    Membuat Component Header & Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="bg-white text-center fixed inset-x-0 bottom-0 z-10">
+    <div
+      class="container mx-auto grid grid-cols-4 gap-4 p-2 sm:w-full md:w-5/12"
+    >
+      <!-- Beranda -->
+      <div>
+        <a
+          href="#"
+          class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center"
+        >
+          <img
+            src="../assets/images/home.png"
+            alt="Home"
+            class="inline-block mb-1"
+            width="25"
+            height="25"
+          />
+          <span class="block text-xs">Beranda</span>
+        </a>
+      </div>
+
+      <!-- Donasi Saya -->
+      <div>
+        <a
+          href="#"
+          class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center"
+        >
+          <img
+            src="../assets/images/heart.png"
+            alt="Heart"
+            class="inline-block mb-1"
+            width="25"
+            height="25"
+          />
+          <span class="block text-xs">Donasi Saya</span>
+        </a>
+      </div>
+
+      <!-- Campaign -->
+      <div>
+        <a
+          href="#"
+          class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center"
+        >
+          <img
+            src="../assets/images/flag.png"
+            alt="Flag"
+            class="inline-block mb-1"
+            width="25"
+            height="25"
+          />
+          <span class="tab tab-kategori block text-xs">Campaign</span>
+        </a>
+      </div>
+
+      <!-- Akun -->
+      <div>
+        <a
+          href="#"
+          class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center"
+        >
+          <img
+            src="../assets/images/user.png"
+            alt="User"
+            class="inline-block mb-1"
+            width="25"
+            height="25"
+          />
+          <span class="block text-xs">Akun</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "FooterComponent",
+  };
+</script>
