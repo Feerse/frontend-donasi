467ac88e36b7fbe26ac0ee0a6344a18f8326ba00
Feerse <sungaikeledang6@gmail.com>
2024-09-23 10:46:39 +0800

    Konfigurasi store Donation Pinia

---
+++ src/stores/donation.js
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+
+export const useDonationStore = defineStore("donation", {
+  state: () => ({
+    //
+  }),
+  getters: {
+    //
+  },
+  actions: {
+    //
+  },
+});
