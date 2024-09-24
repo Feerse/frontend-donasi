f7f149d314cc4bd7fee0a935cb2578018ae59369
Feerse <sungaikeledang6@gmail.com>
2024-09-24 09:32:04 +0800

    Membuat Tampilan Data Donation dari Donatur

--- src/stores/donation.js
+++ src/stores/donation.js
@@ -1,13 +1,68 @@
 import { defineStore } from "pinia";
+import Api from "../api/Api";
 
 export const useDonationStore = defineStore("donation", {
   state: () => ({
-    //
+    // Donations
+    donations: [],
+
+    // Load More
+    nextExist: false,
+    nextPage: 0,
   }),
   getters: {
     //
   },
   actions: {
-    //
+    async getDonation() {
+      try {
+        // Ambil data token dan user
+        const token = localStorage.getItem("token");
+
+        // Set axios header dengan type Authorization + Bearer token
+        Api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+        const response = await Api.get("/donation");
+
+        console.log(response.data.data.data);
+        this.donations = response.data.data.data;
+
+        if (response.data.data.current_page < response.data.data.last_page) {
+          this.nextExist = true;
+          this.nextPage = response.data.data.current_page + 1;
+        } else {
+          this.nextExist = false;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    async getLoadMore(nextPage) {
+      try {
+        // Ambil data token dan user
+        const token = localStorage.getItem("token");
+
+        // Set axios header dengan type Authorization + Bearer token
+        Api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+        // Get data donations dengan parameter page ke server
+        const response = await Api.get(`/donation?page=${nextPage}`);
+
+        response.data.data.data.forEach((row) => {
+          this.donations.push(row);
+        });
+        console.log(response.data.data.data);
+
+        if (response.data.data.current_page < response.data.data.last_page) {
+          this.nextExist = true;
+          this.nextPage = response.data.data.current_page + 1;
+        } else {
+          this.nextExist = false;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 });
