9eb2050347367a2251518e42fc58d3f7f5b45047
Feerse <sungaikeledang6@gmail.com>
2024-09-24 10:20:15 +0800

    Membuat tampilan data Profile

---
+++ src/stores/profile.js
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+import Api from "../api/Api";
+
+export const useProfileStore = defineStore("profile", {
+  state: () => ({
+    profile: {},
+  }),
+  getters: {
+    //
+  },
+  actions: {
+    async getProfile() {
+      try {
+        const token = localStorage.getItem("token");
+        Api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+        const response = await Api.get("/profile");
+
+        this.profile = response.data.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+});
--- src/views/profile/Index.vue
+++ src/views/profile/Index.vue
@@ -1,5 +1,74 @@
 <template>
-  <div class="pb-20 pt-20 text-center">HALAMAN PROFILE</div>
+  <div class="pb-20 pt-20">
+    <div class="container mx-auto grid grid-cols-1 p-3 sm:w-full md:w-5/12">
+      <form method="POST" enctype="multipart/form-data">
+        <div class="bg-white p-5 rounded-md shadow-md mb-5">
+          <div class="flex flex-col justify-center items-center relative">
+            <div>
+              <img
+                :src="profile.avatar"
+                alt="Avatar"
+                class="rounded-full w-28 h-28 object-cover"
+              />
+            </div>
+            <div class="mt-4">
+              <input
+                type="file"
+                class="rounded bg-gray-300 p-2 w-full shadow-sm"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="bg-white p-3 rounded-md shadow-sm">
+          <div class="grid grid-cols-1 gap-4">
+            <div class="mb-2">
+              <label for="name" class="mt-2">Nama Lengkap</label>
+              <input
+                id="name"
+                type="text"
+                class="mt-2 appearance-none w-full bg-gray-200 rounded h-7 shadow-sm placeholder-gray-700 focus:outline-none focus:placeholder-gray-600 focus:bg-gray-300 focus-within:text-gray-600 p-5"
+                placeholder="Nama Lengkap"
+                v-model="profile.name"
+              />
+            </div>
+
+            <div class="mb-2">
+              <label for="email" class="mt-2">Email</label>
+              <input
+                id="email"
+                type="text"
+                class="mt-2 appearance-none w-full bg-gray-200 opacity-70 rounded h-7 shadow-sm placeholder-gray-700 focus:outline-none focus:placeholder-gray-600 focus:bg-gray-300 focus-within:text-gray-600 p-5"
+                placeholder="Email"
+                v-model="profile.email"
+                disabled
+              />
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                class="mt-3 bg-gray-700 text-white p-2 w-full rounded-md shadow-md focus:outline-none"
+              >
+                UPDATE PROFILE
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import { computed, onMounted } from "vue";
+  import { useProfileStore } from "../../stores/profile";
+
+  const profileStore = useProfileStore();
+
+  onMounted(() => {
+    profileStore.getProfile();
+  });
+
+  const profile = computed(() => profileStore.profile);
+</script>
