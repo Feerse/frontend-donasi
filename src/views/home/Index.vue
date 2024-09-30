12825a81118ccfa10ac41499dd9fdb649741ae36
Feerse <sungaikeledang6@gmail.com>
2024-09-30 19:46:12 +0800

    Membuat tampilan component `Slider` dan `Category` pada halaman Homepage

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="pb-20 pt-20 text-center">HALAMAN HOMEPAGE</div>
+  <div class="pb-20 pt-20">
+    <div class="container mx-auto grid grid-cols-1 p-3 sm:w-full md:w-5/12">
+      <!-- Slider -->
+      <div class="grid grid-cols-1 bg-white rounded shadow-md p-1 text-sm">
+        <Slider />
+      </div>
+
+      <!-- CategoryHome -->
+      <CategoryHome />
+
+      <!-- Campaign -->
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import Slider from "../../components/Slider.vue";
+  import CategoryHome from "../../components/CategoryHome.vue";
+</script>
