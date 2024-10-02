06eecb969a6b483ffb5544bcdfebd5a3673901cc
Feerse <sungaikeledang6@gmail.com>
2024-10-02 08:11:38 +0800

    Konfigurasi router untuk Category

--- src/components/CategoryHome.vue
+++ src/components/CategoryHome.vue
@@ -1,61 +1,66 @@
 <template>
-  <div v-if="categories.length > 0">
-    <div class="mt-5 grid grid-cols-4 gap-4 md:gap-4 text-center items-center">
-      <div
-        v-for="category in categories"
-        :key="category.id"
-        class="col-span-2 md:col-span-2 lg:col-span-1 bg-white rounded-md shadow-md p-4 text-center text-xs"
-      >
-        <a href="#">
-          <img
-            :src="category.image"
-            alt="Kategori"
-            width="40"
-            class="inline-block mb-2"
-          />
-          {{ category.name.toUpperCase() }}
-        </a>
-      </div>
-      <div
-        class="col-span-2 md:col-span-1 lg:col-span-1 bg-white rounded-md shadow-md p-4 text-center text-xs"
-      >
-        <a href="#">
-          <img
-            src="../assets/images/menu.png"
-            alt="Menu"
-            width="40"
-            class="inline-block mb-2"
-          />
-          LAINNYA
-        </a>
-      </div>
-    </div>
-  </div>
-  <div v-else>
-    <div class="mt-5 grid grid-cols-4 gap-4 md:gap-4 text-center items-center">
-      <div
-        v-for="i in 4"
-        :key="i"
-        class="sm:col-span-2 md:col-span-2 lg:col-span-2 bg-white rounded-md shadow-md text-center text-xs"
-      >
-        <ContentLoader />
-      </div>
-    </div>
-  </div>
+	<div v-if="categories.length > 0">
+		<div class="mt-5 grid grid-cols-4 gap-4 md:gap-4 text-center items-center">
+			<div
+				v-for="category in categories"
+				:key="category.id"
+				class="col-span-2 md:col-span-2 lg:col-span-1 bg-white rounded-md shadow-md p-4 text-center text-xs"
+			>
+				<router-link
+					:to="{
+						name: 'category.show',
+						params: { slug: category.slug },
+					}"
+				>
+					<img
+						:src="category.image"
+						alt="Kategori"
+						width="40"
+						class="inline-block mb-2"
+					/>
+					{{ category.name.toUpperCase() }}
+				</router-link>
+			</div>
+			<div
+				class="col-span-2 md:col-span-1 lg:col-span-1 bg-white rounded-md shadow-md p-4 text-center text-xs"
+			>
+				<router-link :to="{ name: 'category.index' }">
+					<img
+						src="../assets/images/menu.png"
+						alt="Menu"
+						width="40"
+						class="inline-block mb-2"
+					/>
+					LAINNYA
+				</router-link>
+			</div>
+		</div>
+	</div>
+	<div v-else>
+		<div class="mt-5 grid grid-cols-4 gap-4 md:gap-4 text-center items-center">
+			<div
+				v-for="i in 4"
+				:key="i"
+				class="sm:col-span-2 md:col-span-2 lg:col-span-2 bg-white rounded-md shadow-md text-center text-xs"
+			>
+				<ContentLoader />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
-  import { computed, onMounted } from "vue";
-  import { useCategoryStore } from "../stores/category";
-  import { ContentLoader } from "vue-content-loader";
+	import { computed, onMounted } from "vue";
+	import { useCategoryStore } from "../stores/category";
+	import { ContentLoader } from "vue-content-loader";
 
-  const categoryStore = useCategoryStore();
+	const categoryStore = useCategoryStore();
 
-  // Jalankan action `getCategoryHome()` dari store `category`
-  onMounted(() => {
-    categoryStore.getCategoryHome();
-  });
+	// Jalankan action `getCategoryHome()` dari store `category`
+	onMounted(() => {
+		categoryStore.getCategoryHome();
+	});
 
-  // Get data state `categories` dari store `category`
-  const categories = computed(() => categoryStore.categories);
+	// Get data state `categories` dari store `category`
+	const categories = computed(() => categoryStore.categories);
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -10,75 +10,85 @@ import { useAuthStore } from "../stores/auth";
 
 // Definisikan sebuah rute
 const routes = [
-  {
-    path: "/register",
-    name: "register",
-    component: () => import("../views/auth/Register.vue"),
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: () => import("../views/auth/Login.vue"),
-  },
-  {
-    path: "/dashboard",
-    name: "dashboard",
-    component: () => import("../views/dashboard/Index.vue"),
-    meta: {
-      requiresAuth: true,
-    },
-  },
-  {
-    path: "/donation",
-    name: "donation.index",
-    component: () => import("../views/donation/Index.vue"),
-    meta: {
-      requiresAuth: true,
-    },
-  },
-  {
-    path: "/profile",
-    name: "profile",
-    component: () => import("../views/profile/Index.vue"),
-    meta: {
-      requiresAuth: true,
-    },
-  },
-  {
-    path: "/profile/password",
-    name: "profile.password",
-    component: () => import("../views/profile/Password.vue"),
-    meta: {
-      requiresAuth: true,
-    },
-  },
-  {
-    path: "/",
-    name: "home",
-    component: () => import("../views/home/Index.vue"),
-    meta: {},
-  },
+	{
+		path: "/register",
+		name: "register",
+		component: () => import("../views/auth/Register.vue"),
+	},
+	{
+		path: "/login",
+		name: "login",
+		component: () => import("../views/auth/Login.vue"),
+	},
+	{
+		path: "/dashboard",
+		name: "dashboard",
+		component: () => import("../views/dashboard/Index.vue"),
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
+		path: "/donation",
+		name: "donation.index",
+		component: () => import("../views/donation/Index.vue"),
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
+		path: "/profile",
+		name: "profile",
+		component: () => import("../views/profile/Index.vue"),
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
+		path: "/profile/password",
+		name: "profile.password",
+		component: () => import("../views/profile/Password.vue"),
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
+		path: "/",
+		name: "home",
+		component: () => import("../views/home/Index.vue"),
+		meta: {},
+	},
+	{
+		path: "/category",
+		name: "category.index",
+		component: () => import("../views/category/Index.vue"),
+	},
+	{
+		path: "/category/:slug",
+		name: "category.show",
+		component: () => import("../views/category/Show.vue"),
+	},
 ];
 
 // Buat router
 const router = createRouter({
-  history: createWebHistory(),
-  routes, // <-- routes
+	history: createWebHistory(),
+	routes, // <-- routes
 });
 
 router.beforeEach((to, from, next) => {
-  const authStore = useAuthStore();
+	const authStore = useAuthStore();
 
-  if (to.matched.some((record) => record.meta.requiresAuth)) {
-    // Cek nilai dari getters `isLoggedIn` di module auth
-    if (authStore.isLoggedIn) {
-      next();
-      return;
-    }
-    next("/login");
-  } else {
-    next();
-  }
+	if (to.matched.some((record) => record.meta.requiresAuth)) {
+		// Cek nilai dari getters `isLoggedIn` di module auth
+		if (authStore.isLoggedIn) {
+			next();
+			return;
+		}
+		next("/login");
+	} else {
+		next();
+	}
 });
 
 export default router;
