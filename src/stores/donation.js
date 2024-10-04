4a5bb18509cb79382a8c8b5dcea572b652781c64
Feerse <sungaikeledang6@gmail.com>
2024-10-04 10:32:03 +0800

    Membuat proses donasi

--- src/stores/donation.js
+++ src/stores/donation.js
@@ -2,67 +2,92 @@ import { defineStore } from "pinia";
 import Api from "../api/Api";
 
 export const useDonationStore = defineStore("donation", {
-  state: () => ({
-    // Donations
-    donations: [],
+	state: () => ({
+		// Donations
+		donations: [],
 
-    // Load More
-    nextExist: false,
-    nextPage: 0,
-  }),
-  getters: {
-    //
-  },
-  actions: {
-    async getDonation() {
-      try {
-        // Ambil data token dan user
-        const token = localStorage.getItem("token");
+		// Load More
+		nextExist: false,
+		nextPage: 0,
+	}),
+	getters: {
+		//
+	},
+	actions: {
+		async getDonation() {
+			try {
+				// Ambil data token dan user
+				const token = localStorage.getItem("token");
 
-        // Set axios header dengan type Authorization + Bearer token
-        Api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+				// Set axios header dengan type Authorization + Bearer token
+				Api.defaults.headers.common[
+					"Authorization"
+				] = `Bearer ${token}`;
 
-        const response = await Api.get("/donation");
+				const response = await Api.get("/donation");
 
-        console.log(response.data.data.data);
-        this.donations = response.data.data.data;
+				console.log(response.data.data.data);
+				this.donations = response.data.data.data;
 
-        if (response.data.data.current_page < response.data.data.last_page) {
-          this.nextExist = true;
-          this.nextPage = response.data.data.current_page + 1;
-        } else {
-          this.nextExist = false;
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    },
+				if (
+					response.data.data.current_page <
+					response.data.data.last_page
+				) {
+					this.nextExist = true;
+					this.nextPage = response.data.data.current_page + 1;
+				} else {
+					this.nextExist = false;
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		},
 
-    async getLoadMore(nextPage) {
-      try {
-        // Ambil data token dan user
-        const token = localStorage.getItem("token");
+		async getLoadMore(nextPage) {
+			try {
+				// Ambil data token dan user
+				const token = localStorage.getItem("token");
 
-        // Set axios header dengan type Authorization + Bearer token
-        Api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+				// Set axios header dengan type Authorization + Bearer token
+				Api.defaults.headers.common[
+					"Authorization"
+				] = `Bearer ${token}`;
 
-        // Get data donations dengan parameter page ke server
-        const response = await Api.get(`/donation?page=${nextPage}`);
+				// Get data donations dengan parameter page ke server
+				const response = await Api.get(`/donation?page=${nextPage}`);
 
-        response.data.data.data.forEach((row) => {
-          this.donations.push(row);
-        });
-        console.log(response.data.data.data);
+				response.data.data.data.forEach((row) => {
+					this.donations.push(row);
+				});
+				console.log(response.data.data.data);
 
-        if (response.data.data.current_page < response.data.data.last_page) {
-          this.nextExist = true;
-          this.nextPage = response.data.data.current_page + 1;
-        } else {
-          this.nextExist = false;
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  },
+				if (
+					response.data.data.current_page <
+					response.data.data.last_page
+				) {
+					this.nextExist = true;
+					this.nextPage = response.data.data.current_page + 1;
+				} else {
+					this.nextExist = false;
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		},
+
+		async storeDonation(data) {
+			try {
+				const token = localStorage.getItem("token");
+				Api.defaults.headers.common[
+					"Authorization"
+				] = `Bearer ${token}`;
+
+				const response = await Api.post("/donation", data);
+
+				return response;
+			} catch (error) {
+				throw error.response.data;
+			}
+		},
+	},
 });
--- src/views/donation/Create.vue
+++ src/views/donation/Create.vue
@@ -1,5 +1,77 @@
 <template>
-	<div class="pb-20 pt-20 text-center">HALAMAN BUAT DONASI</div>
+	<div class="pb-20 pt-20">
+		<div class="container mx-auto grid grid-cols-1 p-3 sm:w-full md:w-5/12">
+			<div class="bg-white rounded-md shadow-md p-5">
+				<div class="text-xl">MASUKKAN NOMINAL DONASI</div>
+				<div class="border-2 border-gray-200 mt-3 mb-2"></div>
+
+				<div class="mb-2">
+					<label for="amount">Rp.</label>
+					<input
+						type="number"
+						id="amount"
+						placeholder="0"
+						v-model="donation.amount"
+						class="mt-2 appearance-none w-full bg-gray-200 border border-gray-200 rounded h-15 shadow-sm placeholder-gray-600 focus:outline-none focus:placeholder-gray-600 focus:bg-white focus-within:text-gray-600 p-2 text-right text-xl"
+					/>
+				</div>
+
+				<div class="mb-2">
+					<label for="pray" class="mt-2 font-bold text-lg"
+						>Do'a</label
+					>
+					<textarea
+						id="pray"
+						rows="3"
+						v-model="donation.pray"
+						placeholder="Tulis Do'a / Ucapan"
+						class="mt-2 appearance-none w-full bg-gray-200 border border-gray-200 rounded shadow-sm placeholder-gray-600 focus:outline-none focus:placeholder-gray-600 focus:bg-white focus-within:text-gray-600 p-5"
+					></textarea>
+				</div>
+
+				<button
+					@click="storeDonation"
+					class="mt-4 bg-yellow-500 py-2 rounded-md shadow-md text-base w-full uppercase font-bold focus:outline-none focus:bg-yellow-600"
+				>
+					LANJUT PEMBAYARAN
+				</button>
+			</div>
+		</div>
+	</div>
 </template>
 
-<script setup></script>
+<script setup>
+	import { reactive } from "vue";
+	import { useDonationStore } from "../../stores/donation";
+	import { useRoute, useRouter } from "vue-router";
+	import { useToast } from "vue-toastification";
+
+	const donationStore = useDonationStore();
+	const route = useRoute();
+	const router = useRouter();
+	const toast = useToast();
+
+	// State donation
+	const donation = reactive({
+		amount: 0,
+		pray: "",
+		campaignSlug: route.params.slug, // <-- Data `slug` dari campaign
+	});
+
+	function storeDonation() {
+		if (donation.amount < 10000) {
+			toast.error("Donasi Minimal Rp. 10.000");
+			return false;
+		}
+
+		donationStore
+			.storeDonation(donation)
+			.then(() => {
+				toast.success("Transaksi Berhasil Dibuat!");
+				router.push({ name: "donation.index" });
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+</script>
